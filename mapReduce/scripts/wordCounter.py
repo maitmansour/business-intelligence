3f34f32f38339eed6e76e6ffc07676d8930d9db4
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2019-02-26 15:13:46 +0100

    clean strings before start

--- mapReduce/scripts/wordCounter.py
+++ mapReduce/scripts/wordCounter.py
@@ -23,10 +23,16 @@ class wordCounter(mrs.MapReduce):
         	# Get words (lowercase) from string
             regex = r'\w+'
             wordsList=re.findall(regex,value.lower())
+            # Remove digits
+            wordsList_clean = [x for x in wordsList if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+            # Remove digits within strings
+            wordsList_clean_numbers=[]
+            for x in wordsList_clean:
+            	wordsList_clean_numbers.append(''.join([i for i in x if not i.isdigit()]))
             newKey=1
             print('***** Input Words *****')
-            print(wordsList)            
-            for word in wordsList:
+            print(wordsList_clean_numbers)            
+            for word in wordsList_clean_numbers:
                 yield (word, newKey)
         def reduce(self,key, values):
         	# Return values sum
